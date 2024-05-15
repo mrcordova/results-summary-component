8a79fec317014f03cc9b2b8d176790c4e4c3d54b
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-05-15 11:15:27 -0700

    populate json

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
 import data from '../data.json' assert {type: 'json'};
 
-console.log(data);
+
+window.addEventListener('load', () => {
+
+    let finalScore = 0;
+    
+    for (const item of data) {
+        const divCategory = document.getElementById(`${item.category.toLocaleLowerCase()}`);
+        const imgIcon = divCategory.querySelector('img');
+        const paraTitle = divCategory.querySelector('.icon-con>p');
+        const paraScore = divCategory.querySelectorAll('p')[1].childNodes[0];
+        
+        imgIcon.src = `${item.icon}`;
+        paraTitle.textContent = `${item.category}`;
+        paraScore.textContent = `${item.score}`;
+    
+        finalScore += item.score;
+    }
+    const h1FinalScore = document.querySelector('.final-score-con>h1');
+    
+    h1FinalScore.textContent = `${(finalScore / data.length).toFixed(0)}`;
+})
